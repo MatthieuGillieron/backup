3e0abf870a0acc7908d33f6243aad6c1c877c773
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2025-07-17 16:12:21 +0200

    .

--- src_bonus/parsing/p_map_ok_u.c
+++ src_bonus/parsing/p_map_ok_u.c
@@ -6,7 +6,7 @@
 /*   By: maximemartin <maximemartin@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 09:57:53 by maximemarti       #+#    #+#             */
-/*   Updated: 2025/07/17 16:05:28 by maximemarti      ###   ########.fr       */
+/*   Updated: 2025/07/17 16:11:04 by maximemarti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@ int	is_player(char c)
 	return (c == 'N' || c == 'S' || c == 'E' || c == 'W');
 }
 
-int	is_playable(char c)
+int is_door(char c)
 {
-	return (c == '0' || is_player(c)); // 'D' is not walkable (closed door)
+	return (c == 'D');
+}
+
+int is_playable(char c)
+{
+	return (c == '0' || is_player(c)); // 'D' is not walkable, but valid
 }
