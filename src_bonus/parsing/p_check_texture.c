3abc1d67b13eaa9f89a152360afce91935302b6c
Martin Maxime <149656366+cosmos510@users.noreply.github.com>
2025-07-20 23:25:22 +0200

    update bonus

--- src_bonus/parsing/p_check_texture.c
+++ src_bonus/parsing/p_check_texture.c
@@ -6,7 +6,7 @@
 /*   By: maximemartin <maximemartin@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 10:45:11 by maximemarti       #+#    #+#             */
-/*   Updated: 2025/07/20 23:05:54 by maximemarti      ###   ########.fr       */
+/*   Updated: 2025/07/20 23:14:17 by maximemarti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,7 @@ int	check_file(char **files, t_map_data *map)
 	void	*mlx;
 
 	if (!split_sections(files, map))
-		return (1);
+		cleanup_and_exit(ERR_MAP_MISSING_SECTION, files, map);
 	if (is_map_enclosed(map->map, &map->player, files, map) == -1)
 		cleanup_and_exit(ERR_MAP_MULTI_PLAYER, files, map);
 	if (!is_map_enclosed(map->map, &map->player, files, map))
--- src_bonus/parsing/p_file.c
+++ src_bonus/parsing/p_file.c
@@ -6,7 +6,7 @@
 /*   By: maximemartin <maximemartin@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 17:23:26 by maximemarti       #+#    #+#             */
-/*   Updated: 2025/07/20 23:05:27 by maximemarti      ###   ########.fr       */
+/*   Updated: 2025/07/20 23:24:41 by maximemarti      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ static int	check_header_lines(char **lines, t_map_data *data, int *index)
 		if (!assign_texture_or_color(lines[i], data, &found))
 		{
 			print_error(ERR_MAP_EXTRA_INFO, NULL);
+			free_map_data(data);
 			return (-2);
 		}
 		i++;
@@ -65,23 +66,47 @@ static int	handle_map_parsing(char **lines, t_map_data *data, \
 	return (1);
 }
 
-int	split_sections(char **lines, t_map_data *data)
+static int process_header_sections(char **lines, t_map_data *data, int *i)
 {
-	int	i;
-	int	map_start;
+	int header_result;
 
+	header_result = check_header_lines(lines, data, i);
+	if (header_result == -2)
+	{
+		free_map_data(data);
+		return (0);
+	}
+	if (header_result == 0)
+	{
+		print_error(ERR_MAP_MISSING_SECTION, NULL);
+		free_map_data(data);
+		return (0);
+	}
+	return (1);
+}
+
+static int process_map_section(char **lines, t_map_data *data, int start_index)
+{
+	int map_start;
+
+	map_start = find_map_start(lines, start_index);
+	if (map_start < 0 || !handle_map_parsing(lines, data, start_index, map_start))
+	{
+		print_error(ERR_MAP_INVALID, NULL);
+		free_map_data(data);
+		return (0);
+	}
+	return (1);
+}
+
+int split_sections(char **lines, t_map_data *data)
+{
+	int i;
 	i = 0;
-	if (check_header_lines(lines, data, &i) == -1)
-	{
-		free_map_data(data);
+	if (!process_header_sections(lines, data, &i))
 		return (0);
-	}
-	map_start = find_map_start(lines, i);
-	if (map_start < 0 || !handle_map_parsing(lines, data, i, map_start))
-	{
-		free_map_data(data);
+	if (!process_map_section(lines, data, i))
 		return (0);
-	}
 	return (1);
 }
 
